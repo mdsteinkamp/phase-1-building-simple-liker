4144396709b07ccd09f4e925e47b246c8949dbf2
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-01-15 11:58:57 -0800

    commit tree 3b922effee9ce1472ea2e78d50c74cedee08c0a4

--- main.js
+++ main.js
@@ -4,6 +4,32 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const allHearts = document.querySelectorAll('.like-glyph');
+
+for (const glyph of allHearts) {
+  glyph.addEventListener('click', likeCallback)
+}
+
+function likeCallback(event) {
+  const heart = event.target;
+  if (heart.textContent === FULL_HEART) {
+    heart.textContent = EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+    return
+  }
+  mimicServerCall()
+  .then(function() {
+    console.log(heart)
+    heart.textContent = FULL_HEART; 
+    heart.classList.add('activated-heart')
+  })
+  .catch(function(error) {
+    alert(error)
+    const modal = document.querySelector('#modal')
+    modal.classList.remove('hidden');
+    setTimeout(() => modal.classList.add('hidden'), 3000);
+  })
+}
 
 
 
